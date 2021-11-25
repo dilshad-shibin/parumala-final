2ddc85cf63634e981de04c4eeffc26b20481f4be
dilshad-shibin <msdcva786@gmail.com>
2021-11-25 11:18:58 +0530

    about page modified

--- about.php
+++ about.php
@@ -16,6 +16,7 @@
 
             <!-- banner start -->
             <section class="section about-banner">
+                <div class="innerbanner-overlay"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
@@ -32,7 +33,7 @@
             <section class="about-area section">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-7">
+                        <div class="col-lg-7 order-2-991" >
                             <div class="about-content-area">
                                 <div class="head">
                                     <h1>The Genesis</h1>
@@ -45,9 +46,164 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col-lg-5">
+                        <div class="col-lg-5 order-1-991">
                             <div class="about-img">
                                 <img src="assets/img/about/genesis.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+           <!-- Location area -->
+            <section class="location-area section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <div class="about-count">
+                                <div class="count">
+                                    <h1 class="counter">45</h1>
+                                    <p>Years of Experience</p>
+                                </div>
+                                <div class="count">
+                                    <h1 class="counter counting">500</h1>
+                                    <p>Bedded Hospital</p>
+                                </div>
+                                <div class="count">
+                                    <h1 class="counter">40</h1>
+                                    <p>Specialities</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="location-content">
+                                <h2>Location</h2>
+                                <p>The hospital is located at Parumala, in the vicinity of Parumala Church, which is sanctified by the presence of the tomb of Saint Mar Gregorios Thirumeni, where people from all walks of life throng for solace.</p>
+                                <p>arumala is a small picturesque island surrounded by the tributaries of the Pampa River. It is equidistant from Thiruvalla, Chengannur, Karthikapally and Mavelikkara towns. The place is easily accessible by road, rail and waterways. Thiruvalla, Chengannur, Mavelikkara and Haripad are the nearest railway stations.</p>                                    
+                                <p>Set in this beautiful and serene location, the St. Gregorios Medical Mission Hospital,the St Gregorios International Cancer Care Centre, St.Gregorios International Cardiothoracic Center and Research Institute and the Multi-Specialty Hospital offer best-in-class medical treatment through its various departments.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Management Section -->
+            <section class="mng-section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <div class="mng-head">
+                                <h2>Management & Administration</h2>
+                                <p>St. Gregorios Medical Mission Hospital is owned and controlled by a Charitable Society of Malankara Orthodox Syrian Church, the Head of which is His Holiness Baselius Marthoma Paulose II, the Catholicose of the Apostolic throne of St. Thomas and the Head of Malankara Orthodox Church and managed through a committee called "St. Gregorios Medical Mission Managing Committee" 
+                                constituted as provided in the constitution and memorandum of Charitable Society</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mng-details">
+                                <div class="mng-img">
+                                    <img src="assets/img/about/president.jpg" alt="">
+                                </div>
+                                <div class="mng-text">
+                                    <h1>President <span></span></h1>
+                                    <h4>His Holiness Baselios Marthoma Paulose II</h4>
+                                    <p>Catholicos of the East and Malankara 
+                                        Metropolitan</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mng-details">
+                                <div class="mng-img">
+                                    <img src="assets/img/about/vice.jpg" alt="">
+                                </div>
+                                <div class="mng-text">
+                                    <h1>Vice President <span></span></h1>
+                                    <h4>H.G. Dr. Youhanon Mar 
+                                        Chrysostomos, Metropolitan</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mng-details">
+                                <div class="mng-img">
+                                    <img src="assets/img/about/chief.jpg" alt="">
+                                </div>
+                                <div class="mng-text">
+                                    <h1>Chief Executive Officer <span class="third"></span></h1>
+                                    <h4>Ref.Fr.M.C.Poulose</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Mision-Vission area -->
+            <section class="mision-area section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mision-bx">
+                                <div class="logo">
+                                    <div class="icon"><img src="assets/img/about/mission.png" alt=""></div>
+                                    <div class="head"><h2>Mission</h2></div>
+                                </div>
+                                <div class="text">
+                                    <p>To provide a comprehensive healing environment for our patients and to improve their quality of life.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="mision-bx">
+                                <div class="logo">
+                                    <div class="icon"><img src="assets/img/about/vision.png" alt=""></div>
+                                    <div class="head"><h2>Vision</h2></div>
+                                </div>
+                                <div class="text">
+                                    <p>To be the hospital of choice for patients and health care providers because of our high quality, affordable, compassionate and patient centered care that seeks holistic healing.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- quality row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="quality-area">
+                                <h2>Quality Policy</h2>
+                                <p>We, the management and staff of St. Gregorios Medical Mission Hospital are committed to provide consistent and highest quality of care to deliver the best clinical outcome, patient safety & patient satisfaction.</p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="quality-area" style="margin-top: 0;">
+                                <h2>Values</h2>
+                                <div class="quality-points">
+                                    <ul>
+                                        <li>
+                                            <h3>Respect :</h3>
+                                            <p>Treat everyone in our diverse community, including patients and their family with dignity.</p>
+                                        </li>
+                                        <li>
+                                            <h3>Integrity :</h3>
+                                            <p>Adhere to the highest standards of professionalism, ethics and personal responsibility worthy of the trust our patients place in us.</p>
+                                        </li>
+                                        <li>
+                                            <h3>Compassion :</h3>
+                                            <p>We serve others with empathy, and desire to alleviate their suffering while honoring everyone’s dignity and privacy.</p>
+                                        </li>
+                                    </ul>
+                                    <ul>
+                                        <li>
+                                            <h3>Healing :</h3>
+                                            <p>Inspire hope and nurture the well-being of the whole person respecting physical, emotional and spiritual needs.</p>
+                                        </li>
+                                        <li>
+                                            <h3>Excellence :</h3>
+                                            <p>Deliver the best outcome and highest quality service through the dedicated efforts of every team member.</p>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
