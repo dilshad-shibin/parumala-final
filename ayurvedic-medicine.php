93e486359b223a46d836cbc8b8ea10ee8ba4fcb5
“dilshad-shibin” <msdcva786@gmail.com>
2021-12-04 10:05:20 +0530

    ayurvedic medicine page modified

--- ayurvedic-medicine.php
+++ ayurvedic-medicine.php
@@ -63,13 +63,126 @@
                                 <img src="assets/img/specialities/ayurvedic-medicine.jpg" alt="">
                             </div>
                             <div class="inner-content">
-                                <h1>Infrastructure</h1>
+                                <h1 class="mt-4">Infrastructure</h1>
                                 <p>The hospital building is with air conditioned, non-air conditioned rooms, wards, clinical laboratory, Panchakarma treatment chambers, Dispensary etc. Ecg, X-Ray, MRI’s, OT’s and other lab facilities ensure authentic & perfect investigations. Rehabilitation facilities including Physiotheraphy etc are available. The out patient departments of the Ayurveda Hospital has extensive facility for consultation with expert physicians. Prior appointment can be taken for consultation. OP consultation is available in all the days from 9:00 Am to 4:00 Pm except Sundays. Patients should bring all documents related to his/her ailments. Well trained para medical staff and well equipped classical Ayurveda Pharmacy is on round the clock duty. Procedure for wellness and curative treatments are done ensuring hygiene. Good quality food and pure drinking water is ensured. Excellent ambience providing peace of mind for a healing touch.</p>
-                                <h1>Accomodation</h1>
+                                <h1 class="mt-4">Accomodation</h1>
                                 <p>By providing personalized care by nurses and clinical staffs, we ensure complete privacy and confidentiality of patient records. A detailed prescription with Discharge Summary & diet is given at the time of discharge.</p>
                                 <h1></h1>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Our Teatment Area 2 -->
+            <section class="department-area section treatment-ayurvedic" >
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 text-center">
+                            <div class="dpt-head dpt-head2">
+                                <h2>Treatment Procedures</h2>
+                                <p>All classical treatment procedures which has been written by our great Acharyas are performed classically with outmost care. Some of the treatment procedures are described below :</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>1</span>
+                                </div>
+                                <h3><a href="">Abhyangam </a></h3>
+                                <p>A whole body massage is done with specific herbal oils which is a deeper procedure to remove toxins from body and to revitalize body tissues. This mainly cures obesity, diabetes etc, improves sleep, blood circulation, rejuvenates the whole body, beatifies skin and thus delays aging.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>2</span>
+                                </div>
+                                <h3><a href="">Pizhichil </a></h3>
+                                <p>Medicated oil is poured on to the body in continuous streams and gently massaged for 45 to 60 minutes. It is rejuvenating and strengthening. This is mainly indicated in pramcham, vatarogas, swasa etc and increases body immunity, blood circulation and alleviates burning sensation.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>3</span>
+                                </div>
+                                <h3><a href="">Shirovasti</a></h3>
+                                <p>Ayurvedic herbal oils are poured into a cap fitted on the head for 15 to 60 minutes. This is marvelous for neuralgia, hemicaria, facial paralysis. It increases vitality and body immunity, blood circulation, reduces anxiety & stress.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>4</span>
+                                </div>
+                                <h3><a href="">Kativasti</a></h3>
+                                <p>In this process warm medicated oil is kept over the lower back for 45 minutes to 1 hour. This relieves pain, spinal abnormalities etc.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>5</span>
+                                </div>
+                                <h3><a href="">Podikizhi</a></h3>
+                                <p>Herbal powders (podi) are applied to whole body in boluses with or without medicated oils for 45 miuntes. This relieves pain, stimulates nerve endings, in osteoarthritis, spondylosis etc.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>6</span>
+                                </div>
+                                <h3><a href="">Njavara Kizhi</a></h3>
+                                <p>Specially prepared medicine is tied in muslim bag and is applied on the body. This is applied by 2 to 3 masseurs for 60 minutes. It mainly strengthens the body, improves muscle tone, gains body weight and also disease conditions like paralysis etc.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>7</span>
+                                </div>
+                                <h3><a href="">Dhara</a></h3>
+                                <p>This is a special kerala panchakarma treatment in which medicated oil / buttermilk / milk is poured on to forehead for 35 to 45 minutes. It improves mental state, profound relaxation of mind and body. The medicine changes according to the dosha condition. Sirodhara is good for all ailments, strengthens CNS, sense organs, offers sleep, decreases hypertension, diabetic neuropathy, paralysis, psoriasis etc.
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="featured-block style-two col-lg-4 col-md-6 col-sm-12">
+                            <div class="inner-box">
+                                <div class="image-layer" style="background-image:url(assets/img/feature-2.jpg)"></div>
+                                <div class="icon-box">
+                                    <span>8</span>
+                                </div>
+                                <h3><a href="">Anjanam</a></h3>
+                                <p>Special treatment for eyes are bathed with epical herbal decoction for 15 minutes. It’s good for dry eye, Cataract, glaucoma etc. Other treatments specialized for eyes are putapaleam, Tarpanam (medicated ghee is poured into eyes) etc.
+                                </p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </section>
@@ -96,16 +209,16 @@
             <section class="dept-doctors-area">
                 <div class="container">
                     <div class="row">
-                        <div class="col-12">
-                            <div class="doctor-slider2 owl-carousel">
+                        <div class="col-lg-8">
+                            <div class="doctor-slider4 owl-carousel">
 
                                 <div class="single-doctor">
                                     <div class="doctor-photo">
-                                        <img src="assets/img/doctors/ajiths-sunny.jpg" alt="">
+                                        <img src="assets/img/doctors/john-george.jpg" alt="">
                                     </div>
                                     <div class="doctor-info">
-                                        <h5 class="name">Dr.Ajith Sunny</h5>
-                                        <p>Senior Consultant & HOD Anesthesia</p>
+                                        <h5 class="name">Dr.John K George</h5>
+                                        <p>Senior Consultant Ayurveda</p>
                                         <div class="dr-appointment">
                                             <div class="apt"><h3>Appointment</h3></div>
                                             <div class="call"><a href="tel:+919656127678"><i class="fas fa-phone-alt"></i></a></div>
@@ -115,11 +228,11 @@
 
                                 <div class="single-doctor">
                                     <div class="doctor-photo">
-                                        <img src="assets/img/doctors/jithin-cherian.jpg" alt="">
+                                        <img src="assets/img/doctors/teney-ann.jpg" alt="">
                                     </div>
                                     <div class="doctor-info">
-                                        <h5 class="name">Dr.Jithin J Cherian</h5>
-                                        <p>Consultant Anaesthesiologist</p>
+                                        <h5 class="name">Dr.Teney Ann Vijoy</h5>
+                                        <p>Junior Consultant Ayurveda</p>
                                         <div class="dr-appointment">
                                             <div class="apt"><h3>Appointment</h3></div>
                                             <div class="call"><a href="tel:+919656127678"><i class="fas fa-phone-alt"></i></a></div>
@@ -127,19 +240,19 @@
                                     </div>
                                 </div>
 
-                                <div class="single-doctor">
+                                <!-- <div class="single-doctor">
                                     <div class="doctor-photo">
-                                        <img src="assets/img/doctors/greeshma.jpg" alt="">
+                                        <img src="assets/img/doctors/renal/praveen.jpg" alt="">
                                     </div>
                                     <div class="doctor-info">
-                                        <h5 class="name">Dr.Greeshma S Lekshmy</h5>
-                                        <p>Consultant Anaesthesiologist</p>
+                                        <h5 class="name">Dr.Praveen Gopi</h5>
+                                        <p>Consultant Urologist</p>
                                         <div class="dr-appointment">
                                             <div class="apt"><h3>Appointment</h3></div>
                                             <div class="call"><a href="tel:+919656127678"><i class="fas fa-phone-alt"></i></a></div>
                                         </div>
                                     </div>
-                                </div>
+                                </div> -->
 
                             </div>
                         </div>
